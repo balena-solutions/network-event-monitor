1606052a1c5098e5a31e37048c0d3565e4dbec8c
Edwin Joassart <edwin@blck.coffee>
2026-03-25 14:10:56 +0100

    patch: add dbus security notice

--- boot_partition/50-event-emit.sh
+++ boot_partition/50-event-emit.sh
@@ -4,6 +4,8 @@ INTERFACE=$1
 ACTION=$2
 
 # Set the dbus system bus address
+# Security Warning: This script uses the system bus, which can have security implications. 
+# Ensure that the system bus is properly secured and that only trusted applications can send signals to it.
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
 # Publish the interface and action to dbus
